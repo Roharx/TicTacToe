01164cc30ab279645c435d96fab4e8a4193e0d0f
Bálint <47783658+Roharx@users.noreply.github.com>
2022-10-11 12:31:23 +0200

    Fixed original spelling mistakes
    Fixed the spelling mistakes in the comments and @Params

--- TicTacToe_SourceOnly/tictactoe/bll/GameBoard.java
+++ TicTacToe_SourceOnly/tictactoe/bll/GameBoard.java
@@ -13,15 +13,16 @@ import javafx.scene.control.Button;
  */
 public class GameBoard implements IGameModel
 {
-    /**
-     * Returns 0 for player 0, 1 for player 1.
-     *
-     * @return int Id of the next player.
-     */
+
     int row = 3, col = 3;
     private int turnsLeft = row * col;
     private boolean turnOfPlayerZero = true;
 
+    /**
+     * Returns 0 for player 0, 1 for player 1.
+     *
+     * @return int ID of the next player.
+     */
     public int getNextPlayer()
     {
         if(turnOfPlayerZero)
@@ -30,10 +31,10 @@ public class GameBoard implements IGameModel
     }
 
     /**
-     * Attempts to let the current player play at the given coordinates. It the
-     * attempt is succesfull the current player has ended his turn and it is the
+     * Attempts to let the current player play at the given button. If the
+     * attempt is successful the current player has ended his turn, and it is the
      * next players turn.
-     * @param btn gets the button pressed
+     * @param btn gets the button pressed by the user
      * @return true if the move is accepted, otherwise false. If gameOver == true
      * this method will always return false.
      */
